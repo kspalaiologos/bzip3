df301ae82f533dbd4a65cca74ec2ce4d734d7266
Kamila Szewczyk <kspalaiologos@gmail.com>
2022-05-10 13:01:00 +0200

    relicense to LGPLv3

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
+
 /*
  * BZip3 - A spiritual successor to BZip2.
  * Copyright (C) 2022 Kamila Szewczyk
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
+ * under the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
  *
@@ -12,7 +13,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU General Public License along with
+ * You should have received a copy of the GNU Lesser General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
